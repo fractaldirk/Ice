2377e4d5c518d8fae135281345dc801d76fc1175
fractal <circle@ubuntu.(none)>
2013-02-08 18:45:18 +0100

    update little

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,7 @@ module CommentsHelper
   def activity_description
     "#{current_user.user_name} commented the following on #{@post.title}:"
   end
+  def wall_activity_description
+    "#{current_user.user_name} shared the following on #{@post.title}:"
+  end
 end
